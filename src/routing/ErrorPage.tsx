bed2476a7703e3fc79c7d40241b9369cdc66dffc
gaulepal <gaulepal@gmail.com>
2023-09-22 14:12:10 +0200

    add conditional route error check

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,8 +1,25 @@
+import { Link, isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
 const ErrorPage = () => {
+  const routeError = useRouteError();
+  console.log('routeError::', routeError);
+
+  const isRouteError = isRouteErrorResponse(routeError);
+  // isRouteError returns true that means User tries to access invalid route
+
   return (
     <>
       <h1 className='text-red-400 text-3xl'>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p className='mb-4'>
+        {isRouteError
+          ? 'Invalid Route'
+          : 'Sorry, an unexpected error has occurred.'}
+      </p>
+      {isRouteError && (
+        <Link to='/' className='bg-green-300 px-4 py-2 rounded-md mt-8'>
+          Homepage
+        </Link>
+      )}
     </>
   );
 };
