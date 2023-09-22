6819fe7b76443f7b0bf0a011f71e706db7ee384a
gaulepal <gaulepal@gmail.com>
2023-09-22 14:03:03 +0200

    add style to the error page

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,7 +1,7 @@
 const ErrorPage = () => {
   return (
     <>
-      <h1>Oops...</h1>
+      <h1 className='text-red-400 text-3xl'>Oops...</h1>
       <p>Sorry, an unexpected error has occurred.</p>
     </>
   );
