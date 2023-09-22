11d6e03e82c785e2608c64f876e7e37915ef6d8c
gaulepal <gaulepal@gmail.com>
2023-09-22 13:54:16 +0200

    add condition to apply the selected link

--- src/routing/NavBar.tsx
+++ src/routing/NavBar.tsx
@@ -1,12 +1,25 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav className='w-full flex text-zinc-800'>
-      <Link to='/' className='mr-8 pr-6'>
+      {/* <Link to='/' className='mr-8 pr-6'> */}
+      <NavLink
+        to='/'
+        className={({ isActive }) =>
+          isActive ? 'text-green-400 font-bold' : ''
+        }
+      >
         Home
-      </Link>
-      <Link to='/users'>Users</Link>
+      </NavLink>
+      <NavLink
+        to='/users'
+        className={({ isActive, isPending }) =>
+          isPending ? 'bg-red-400' : isActive ? 'text-green-400 font-bold' : ''
+        }
+      >
+        Users
+      </NavLink>
     </nav>
   );
 };
